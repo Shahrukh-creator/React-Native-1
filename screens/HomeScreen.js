c1661d61eebfff6f18998eaa8e8248f5a4030793
Shahrukh-creator <shahrukhshahif@gmail.com>
2021-11-03 12:55:46 +0500

    Updated

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,27 +1,62 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import {Button, View, Text, StyleSheet} from 'react-native';
+import {Card} from 'react-native-elements';
 
-function HomeScreen({ navigation }) {
+function HomeScreen({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to ProfileScreen"
-        onPress={() => navigation.navigate('ProfileStack')}
-      />
-       <Button
-        title="Go to ReduxScreen"
-        onPress={() => navigation.navigate('ReduxScreen')}
-      />
-       <Button
-        title="Go to MapView"
-        onPress={() => navigation.navigate('MapView')}
-      />
-      <Button
-        title="Go to HomeScreen"
-        onPress={() => navigation.navigate('Home')}
-      />
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Card>
+        <Card.Title>ProfileScreen</Card.Title>
+        <Card.Divider />
+
+        <Button
+          title="ProfileScreen"
+          onPress={() => navigation.navigate('ProfileStack')}
+        />
+      </Card>
+      <View style={styles.buttonSpace}></View>
+
+      <Card>
+        <Card.Title>ReduxScreen</Card.Title>
+        <Card.Divider />
+
+        <Button
+          title="ReduxScreen"
+          onPress={() => navigation.navigate('ReduxScreen')}
+        />
+      </Card>
+      <View style={styles.buttonSpace}></View>
+
+      <Card>
+        <Card.Title>HomeScreen</Card.Title>
+        <Card.Divider />
+
+        <Button
+          title="HomeScreen"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </Card>
+
+      <View style={styles.buttonSpace}></View>
+
+      <Card>
+        <Card.Title>MapScreen</Card.Title>
+        <Card.Divider />
+
+        <Button
+          title="MapScreen"
+          onPress={() => navigation.navigate('MapView')}
+        />
+      </Card>
+
+      <View style={styles.buttonSpace}></View>
+
     </View>
   );
 }
 export default HomeScreen;
+const styles = StyleSheet.create({
+  buttonSpace: {
+    marginBottom: 20,
+  },
+});
